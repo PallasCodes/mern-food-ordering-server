87d3b29b714398b404a613afbb53e7fc881939ed
pallascodes <pallascodes@gmail.com>
2025-01-08 12:05:18 -0600

    get user api added

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -16,8 +16,8 @@ const handleValidationErrors = async (
 
 export const validateMyUserRequest = [
   body('name').isString().notEmpty().withMessage('Name must be a string'),
-  body('adressLine1').isString().notEmpty().withMessage('AddressLine1 must be a string'),
+  body('addressLine1').isString().notEmpty().withMessage('AddressLine1 must be a string'),
   body('city').isString().notEmpty().withMessage('City must be a string'),
-  body('county').isString().notEmpty().withMessage('Country must be a string'),
+  body('country').isString().notEmpty().withMessage('Country must be a string'),
   handleValidationErrors,
 ]
